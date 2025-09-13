fd9cbaf71a572c3719f1377ccedafdfe5a397861
anamikhoirul88 <anamikhoirul88@gmail.com>
2025-09-13 11:52:44 +0700

    250441100044_khoirulanami

---
+++ 250441100044_KHOIRUL ANAMI_D1/PERTEMUAN 1.py
@@ -0,0 +1,39 @@
+# # # nama = "Khoirul anami"
+# # # nim = 250441100044
+# # # semester = 1
+# # # hobi ="membaca buku dan mendengarkan musik"
+# # # cita_cita =" dosen sekaligus pengusaha hebat"
+
+
+# # # print("====BIODATA MAHASISWA====")
+# # # print("Nama  saya: "+ nama)
+# # # print(f"Nim saya: {nim}")
+# # # print("Saya sekarang berkuliah di semester : {}".format(semester))
+# # # print(f"hobi saya adalah:"+hobi)
+# # # print(f"cita cita saya adalah seorang:"+cita_cita)
+
+# # a = 100
+# # b = 50
+# # c = a + b
+# # d = a - b
+# # e = a * b
+# # f = a / b
+# # g = a % b
+
+# # print("Hasil penjumlahan:" ,c)
+# # print("Hasil pengurangan:", d)
+# # print("Hasil perkalian:", e)
+# # print("Hasil pemabagian:", f)
+# # print("Hasil modulus:", g)
+
+# nama = input ("Silahkan masukkan nama anda:" )
+# umur = input ("masukkan umur anda:" )
+# buah =input ("masukkan buah faforit anda" )
+
+
+# print ("Nama saya adalah: " + nama)
+# print ("umur saya adalah: "+ umur +" tahun")
+# print ("Buah favorit saya adalah: "+ buah)
+# print ("okey sekarang anda berhasil")
+
+
